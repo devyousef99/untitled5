76bb4fa3501018c8e40317b95931da413b16a6d5
YaraAbdulaziz <yarasembilan@gmail.com>
2021-11-30 14:39:18 +0300

    Add language translations

---
+++ lib/translations/codegen_loader.g.dart
@@ -0,0 +1,94 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> ar = {
+  "First_Name": "الإسم الأول",
+  "Last_Name": "الإسم الأخير",
+  "Phone_Number": "رقم الجوال",
+  "Email_Address": "البريد الإلكتروني",
+  "Male": "ذكر",
+  "Female": "أنثى",
+  "Date_Of_Birthday": "تاريخ الميلاد",
+  "profile": "الصفحة الشخصية",
+  "Save_Button": "حفظ",
+  "Select_date_Button": "اختر التاريخ",
+  "errorFormatText_profile": "الرجاء إدخال التاريخ بشكل صحيح",
+  "errorInvalidText_profile": "الرجاء إدخال التاريخ بشكل صحيح",
+  "snackBar_message_profile": "آسفين!, لازم تكون أكبر من 15",
+  "return_ValidateMobile_profile": "الرجاء إدخال رقم جوال صالح",
+  "return_ValidateName_profile": "الرجاء إدخال الإسم بشكل صحيح",
+  "return_ValidateEmail_profile": "الرجاء إدخال بريد الكتروني صالح",
+  "home_page": "الصفحة الرئيسية",
+  "MyPlan_page": "خطتي",
+  "Map_page": "الخريطة",
+  "Events_page": "الفعاليات",
+  "dropDownButton_landing": "وش جوك ؟",
+  "Artists": "الفنانين",
+  "login_page": "تسجيل الدخول",
+  "Password": "رمز المرور",
+  "forget_password_button": "نسيت كلمة المرور",
+  "mingle_landing": "وناسة",
+  "Listen_landing": "دندنة",
+  "PhotoSpots_landing": "تصوير",
+  "Chill_landing": "رواق",
+  "Eat_landing": "عشوة",
+  "Dance_landing": "رقص وهيصة",
+  "createAccount_loginPage": "إنشاء حساب",
+  "signup_button": "تسجيل",
+  "title_AlertDialog_signup": "تفقّد بريدك الإلكتروني",
+  "content_AlertDialog_signup": "لقد قمنا بإرسال بريد إلكتروني يحتوي على رابط للتحقق من عنوان بريدك الإلكتروني.",
+  "RaisedButton_AlertDialog_signup": "حسنًا"
+};
+static const Map<String,dynamic> en = {
+  "First_Name": "First Name",
+  "Last_Name": "Last Name",
+  "Phone_Number": "Phone Number",
+  "Email_Address": "Email Address",
+  "Male": "Male",
+  "Female": "Female",
+  "Date_Of_Birthday": "Date Of Birthday",
+  "profile": "Profile",
+  "Save_Button": "Save",
+  "Select_date_Button": "Select date",
+  "errorFormatText_profile": "Please enter a valid date",
+  "errorInvalidText_profile": "Please enter a valid date",
+  "snackBar_message_profile": "Sorry!, you must be 15 years old or over",
+  "return_ValidateMobile_profile": "Please enter a valid phone number",
+  "return_ValidateName_profile": "Please enter your name",
+  "return_ValidateEmail_profile": "Please enter a valid email address",
+  "home_page": "Home",
+  "MyPlan_page": "My Plan",
+  "Map_page": "Map",
+  "Events_page": "Events",
+  "dropDownButton_landing": "What Are You In The Mood For ?",
+  "Artists": "Artists",
+  "login_page": "Login",
+  "Password": "Password",
+  "forget_password_button": "Forget Password",
+  "mingle_landing": "Mingle",
+  "Listen_landing": "Listen",
+  "PhotoSpots_landing": "Photo Spots",
+  "Chill_landing": "Chill",
+  "Eat_landing": "Eat",
+  "Dance_landing": "Dance",
+  "createAccount_loginPage": "Create Account",
+  "signup_button": "Sign up",
+  "title_AlertDialog_signup": "Check your email",
+  "content_AlertDialog_signup": "To confirm your email address.",
+  "RaisedButton_AlertDialog_signup": "Ok"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"ar": ar, "en": en};
+}
---
+++ lib/translations/locale_keys.g.dart
@@ -0,0 +1,41 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const First_Name = 'First_Name';
+  static const Last_Name = 'Last_Name';
+  static const Phone_Number = 'Phone_Number';
+  static const Email_Address = 'Email_Address';
+  static const Male = 'Male';
+  static const Female = 'Female';
+  static const Date_Of_Birthday = 'Date_Of_Birthday';
+  static const profile = 'profile';
+  static const Save_Button = 'Save_Button';
+  static const Select_date_Button = 'Select_date_Button';
+  static const errorFormatText_profile = 'errorFormatText_profile';
+  static const errorInvalidText_profile = 'errorInvalidText_profile';
+  static const snackBar_message_profile = 'snackBar_message_profile';
+  static const return_ValidateMobile_profile = 'return_ValidateMobile_profile';
+  static const return_ValidateName_profile = 'return_ValidateName_profile';
+  static const return_ValidateEmail_profile = 'return_ValidateEmail_profile';
+  static const home_page = 'home_page';
+  static const MyPlan_page = 'MyPlan_page';
+  static const Map_page = 'Map_page';
+  static const Events_page = 'Events_page';
+  static const dropDownButton_landing = 'dropDownButton_landing';
+  static const Artists = 'Artists';
+  static const login_page = 'login_page';
+  static const Password = 'Password';
+  static const forget_password_button = 'forget_password_button';
+  static const mingle_landing = 'mingle_landing';
+  static const Listen_landing = 'Listen_landing';
+  static const PhotoSpots_landing = 'PhotoSpots_landing';
+  static const Chill_landing = 'Chill_landing';
+  static const Eat_landing = 'Eat_landing';
+  static const Dance_landing = 'Dance_landing';
+  static const createAccount_loginPage = 'createAccount_loginPage';
+  static const signup_button = 'signup_button';
+  static const title_AlertDialog_signup = 'title_AlertDialog_signup';
+  static const content_AlertDialog_signup = 'content_AlertDialog_signup';
+  static const RaisedButton_AlertDialog_signup = 'RaisedButton_AlertDialog_signup';
+
+}
